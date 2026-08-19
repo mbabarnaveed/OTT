d636330d362f2f052d629311f0a55434a2ff1ee3
babarnaveed <babar.naveed@starzplayarabia.com>
2026-08-19 18:41:25 +0400

    add files

--- OTT_APP/apps/tv/src/main/kotlin/com/ott/tv/MainActivity.kt
+++ OTT_APP/apps/tv/src/main/kotlin/com/ott/tv/MainActivity.kt
@@ -8,16 +8,30 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import com.ott.common.routes.EntryBuilder
+import com.ott.common.routes.NavigationManager
+import com.ott.tv.AppRoot
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity: ComponentActivity()  {
+
+    @Inject
+    lateinit var entryBuilders: Set<EntryBuilder>
+
+    @Inject
+    lateinit var navigationManager: NavigationManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            Box(modifier = Modifier.fillMaxSize().background(color = Color.White)) { }
+            AppRoot(
+                intent = intent,
+                entryBuilders=entryBuilders,
+                navigationManager = navigationManager
+            )
         }
     }
 }
