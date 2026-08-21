7ee62a5e5e9d81fabd64039dea76cc0c1b55038a
babarnaveed <babar.naveed@starzplayarabia.com>
2026-08-21 16:42:32 +0400

    add screens

--- OTT_APP/core/common/src/main/kotlin/com/ott/common/viewmodel/GlobalPageViewModel.kt
+++ OTT_APP/core/common/src/main/kotlin/com/ott/common/viewmodel/GlobalPageViewModel.kt
@@ -3,8 +3,6 @@ package com.ott.common.viewmodel
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import com.ott.common.ui.screen.ScreenState
-import com.ott.common.viewmodel.startup.StartupBaseViewModel
-import com.ott.session.manager.SessionManager
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.Flow
@@ -14,7 +12,6 @@ import javax.inject.Inject
 @HiltViewModel
 open class GlobalPageViewModel @Inject constructor(
     @param:ApplicationContext private val context: Context,
-    private val appSessionManager: SessionManager
 ): ViewModel(){
     protected val _screenStateFlow = MutableStateFlow<ScreenState>(ScreenState.Loading)
     val screenStateFlow: Flow<ScreenState> = _screenStateFlow
--- OTT_APP/core/common/src/main/kotlin/com/ott/common/viewmodel/startup/StartupBaseViewModel.kt
+++ OTT_APP/core/common/src/main/kotlin/com/ott/common/viewmodel/startup/StartupBaseViewModel.kt
@@ -2,7 +2,6 @@ package com.ott.common.viewmodel.startup
 
 import android.content.Context
 import com.ott.common.viewmodel.GlobalPageViewModel
-import com.ott.session.manager.SessionManager
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
@@ -10,7 +9,6 @@ import javax.inject.Inject
 @HiltViewModel
 open class StartupBaseViewModel @Inject constructor(
     @param:ApplicationContext private val context: Context,
-    private val appSessionManager: SessionManager
-): GlobalPageViewModel(context=context, appSessionManager=appSessionManager) {
+): GlobalPageViewModel(context=context) {
 
 }
