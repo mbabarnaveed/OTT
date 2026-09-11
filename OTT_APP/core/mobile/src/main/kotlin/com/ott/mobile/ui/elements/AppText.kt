3a3e60ecdc53f7d4c16dbf3cded3c621007eb450
babarnaveed <babar.naveed@starzplayarabia.com>
2026-09-11 12:31:45 +0400

    add files

---
+++ OTT_APP/core/mobile/src/main/kotlin/com/ott/mobile/ui/elements/AppText.kt
@@ -0,0 +1,19 @@
+package com.ott.mobile.ui.elements
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.ott.design_system.AppTheme
+import com.ott.design_system.AppTypography
+
+
+@Composable
+fun AppText(
+    modifier: Modifier = Modifier,
+    text: String,
+    color: Color = AppTheme.colorScheme.body,
+    style: AppTypography = AppTheme.typography,
+
+    ) {
+
+}
